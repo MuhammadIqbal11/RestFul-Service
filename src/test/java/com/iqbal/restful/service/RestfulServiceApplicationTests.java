d212ef53702c8118669f8dc6a606cd26622e4cd1
Muhammad Iqbal <113502513+MuhammadIqbal11@users.noreply.github.com>
2022-09-27 00:15:46 +0700

    Add files via upload

---
+++ src/test/java/com/iqbal/restful/service/RestfulServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iqbal.restful.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestfulServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
